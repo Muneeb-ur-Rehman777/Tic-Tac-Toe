c57ff5ee7bed68bdfa6eaf4d4953ae8839a262d6
Muneeb-ur-Rehman777 <ch361054@gmail.com>
2026-02-22 17:38:33 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import { FaPartyHorn } from "react-icons/fa";
 
 function App() {
   let winPatterns = [
@@ -60,12 +61,12 @@ function App() {
             if (newInput[pattern[0]] !== "") {
               if (newInput[pattern[0]] == newInput[pattern[1]] && newInput[pattern[1]] == newInput[pattern[2]]) {
                 if (newInput[pattern[0]] == "X") {
-                  setwinner("player 1 Wins")
+                  setwinner(`${name1} Wins`)
                   setwined(true)
                   setplayer1count(prev => prev + 0.5)
                 }
                 else {
-                  setwinner("player 2 Wins")
+                  setwinner(`${name2} Wins`)
                   setwined(true)
                   setplayer2count(prev => prev + 0.5)
                 }
@@ -92,12 +93,12 @@ function App() {
             if (newInput[pattern[0]] !== "") {
               if (newInput[pattern[0]] == newInput[pattern[1]] && newInput[pattern[1]] == newInput[pattern[2]]) {
                 if (newInput[pattern[0]] == "O") {
-                  setwinner("player 2 Wins")
+                  setwinner(`${name2} Wins`)
                   setwined(true)
                   setplayer2count(prev => prev + 0.5)
                 }
                 else {
-                  setwinner("player 1 Wins")
+                  setwinner(`${name1} Wins`)
                   setwined(true)
                   setplayer1count(prev => prev + 0.5)
                 }
@@ -130,6 +131,19 @@ function App() {
     setwined(false)
 
   }
+  const [name2, setname2] = useState("Player 2")
+  const [name1, setname1] = useState("Player 1");
+
+  function changeName1() {
+    let newname1 = prompt("Enter name of Player 1(X)")
+    setname1(newname1 || "Player 1")
+  }
+
+  function changeName2() {
+    let newname2 = prompt("Enter name of Player 2(O)")
+    setname2(newname2 || "Player 2")
+
+  }
 
 
 
@@ -137,7 +151,8 @@ function App() {
   return (
     <>
       <div className='Body'>
-        <h1>{winner}</h1>
+        <h1 className="winner">{winner || "Welcome to Tic-Tac-Toe!" }</h1>
+        <h1 className="turn">Its {player0 ? name1 : name2}'s Turn</h1>
         <div className='box'>
           <h1 className="heading">Tic-Tac-Toe</h1>
           <div className="game">
@@ -147,8 +162,8 @@ function App() {
           </div>
         </div>
         <div className="players">
-          <button className="pl">Player 1 = {player1count}</button>
-          <button className="pl">Player 2 = {player2count}</button>
+          <button className="pl" onClick={changeName1}>{name1} = {player1count}</button>
+          <button className="pl" onClick={changeName2}>{name2} = {player2count}</button>
         </div>
         <div className="agian">
           <button className="reset" onClick={reset}>Reset</button>
